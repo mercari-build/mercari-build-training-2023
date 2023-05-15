ab4832e843efad37d3fec4bfecd0cd2966c3d33d
kotapiku <ice08235523@gmail.com>
2023-05-16 00:16:08 +0900

    feat: show images of items

--- typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
@@ -1,65 +1,65 @@
 import React, { useEffect, useState } from 'react';
 
 interface Item {
-  id: number;
-  name: string;
-  category: string;
-  image_filename: string;
+    id: number;
+    name: string;
+    category: string;
+    image_filename: string;
 };
 
 const server = process.env.REACT_APP_API_URL || 'http://127.0.0.1:9000';
 const placeholderImage = process.env.PUBLIC_URL + '/logo192.png';
 
 interface Prop {
-  reload?: boolean;
-  onLoadCompleted?: () => void;
+    reload?: boolean;
+    onLoadCompleted?: () => void;
 }
 
 export const ItemList: React.FC<Prop> = (props) => {
-  const { reload = true, onLoadCompleted } = props;
-  const [items, setItems] = useState<Item[]>([])
-  const fetchItems = () => {
-    fetch(server.concat('/items'),
-      {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('GET success:', data);
-        setItems(data.items);
-        onLoadCompleted && onLoadCompleted();
-      })
-      .catch(error => {
-        console.error('GET error:', error)
-      })
-  }
-
-  useEffect(() => {
-    if (reload) {
-      fetchItems();
+    const { reload = true, onLoadCompleted } = props;
+    const [items, setItems] = useState<Item[]>([])
+    const fetchItems = () => {
+        fetch(server.concat('/items'),
+            {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('GET success:', data);
+                setItems(data.items);
+                onLoadCompleted && onLoadCompleted();
+            })
+            .catch(error => {
+                console.error('GET error:', error)
+            })
     }
-  }, [reload]);
 
-  return (
-    <div>
-      {items.map((item) => {
-        return (
-          <div key={item.id} className='ItemList'>
-            {/* TODO: Task 1: Replace the placeholder image with the item image */}
-            <img src={placeholderImage} />
-            <p>
-              <span>Name: {item.name}</span>
-              <br />
-              <span>Category: {item.category}</span>
-            </p>
-          </div>
-        )
-      })}
-    </div>
-  )
+    useEffect(() => {
+        if (reload) {
+            fetchItems();
+        }
+    }, [reload]);
+
+    return (
+        <div>
+            {items.map((item) => {
+                console.log(item.image_filename);
+                return (
+                    <div key={item.id} className='ItemList'>
+                        <img src={server.concat('/image/', item.image_filename)} height="200" />
+                        <p>
+                            <span>Name: {item.name}</span>
+                            <br />
+                            <span>Category: {item.category}</span>
+                        </p>
+                    </div>
+                )
+            })}
+        </div>
+    )
 };
