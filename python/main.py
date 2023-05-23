c3e7e406aa27b87818d9f216007f683fbfe98374
Lai <eeeiren@gmail.com>
2023-05-23 18:07:48 +0900

    Database modification

--- python/main.py
+++ python/main.py
@@ -1,7 +1,9 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException
+import sqlite3
+import hashlib
+from fastapi import FastAPI, Form, HTTPException, status, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -23,13 +25,50 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
+async def add_item(name: str = Form(...),
+                   category: str = Form(...),
+                   image: UploadFile = File(...)):
+    ALLOWED_IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png']
+    def is_image_file(filename: str) -> bool:
+        extension = os.path.splitext(filename)[1].lower()
+        return extension in ALLOWED_IMAGE_EXTENSIONS
+    if not is_image_file(image.filename):
+        raise HTTPException(status_code=400, detail="Invalid image file")
+    
+    image_bytes = await image.read() # turn the image into a buffer-like object
+    image_hashed = hashlib.sha256(image_bytes).hexdigest()  # hash the image
+    new_image_name = image_hashed + ".jpg"
+    with open(f"./images/{new_image_name}", "wb") as f:
+        f.write(image_bytes)  # write the hashed image to a file
+    
+    connection = sqlite3.connect("../db/mercari.sqlite3") # connect to database
+    cursor = connection.cursor()
+
+    category_sql = "INSERT INTO category (name) VALUES (?)"
+    cursor.execute(category_sql, (category,))
+
+    sql = "INSERT INTO items (name, category_id, image_name) VALUES (?, ?, ?)"
+    new_item = (name, category, new_image_name)
+    cursor.execute(sql, new_item)
+    connection.commit()
+
     logger.info(f"Receive item: {name}")
     return {"message": f"item received: {name}"}
 
+@app.get("/items")
+def listed_items():
+    return items
+
+@app.get("/items/{id}")
+def item_details(id: str):
+    for item in items_list:
+        if item["id"] == id:
+            return item
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
-    # Create image path
+# Create image path
     image = images / image_filename
 
     if not image_filename.endswith(".jpg"):
@@ -38,5 +77,4 @@ async def get_image(image_filename):
     if not image.exists():
         logger.debug(f"Image not found: {image}")
         image = images / "default.jpg"
-
     return FileResponse(image)
